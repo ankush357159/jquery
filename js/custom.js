4c6d9984f67ff982b6946a8c3a03bf8fa9862747
Ankush <ankush357159@gmail.com>
2022-05-14 04:53:18 +0530

    jquery events

---
+++ js/custom.js
@@ -0,0 +1,87 @@
+// jQuery("button").click(function() {
+//     console.log("Button clicked")
+// } )
+// $("button").click(function() {
+//     console.log("Button clicked")
+// })
+// jQuery.noConflict()
+// jQuery(document).ready(function($) {
+//     $("button").click(function() {
+//         console.log("Button clicked when page is ready")
+//     })
+// })
+
+
+jQuery.noConflict()
+// jQuery(document).ready(function($) {
+//     $("button").click(function() {
+//         console.log("Button is clicked")
+//     })
+
+//     $("p").click(function() {
+//         alert("Element Selector")
+//     })
+
+//     $("#btn-id").click(function() {
+//         alert("Id Selector")
+//     })
+
+//     $(".btn-class").click(function() {
+//         alert("Class Selector")
+//     })
+// })
+
+// jQuery(document).ready(function($) {
+//     // Mouse Events
+//     $("p").click(function() {
+//         console.log("Clicked")
+//     })
+
+//     $("p").dblclick(function() {
+//         console.log("Double clicked")
+//     })
+
+//     $("p").mouseenter(function() {
+//         console.log("Mouse Entered")
+//     })
+
+//     $("p").mouseleave(function() {
+//         console.log("Mouse left")
+//     })
+
+
+//     // Keyboard events
+//     $("#name").keypress(function() {
+//         console.log("Key pressed")
+//     })
+
+//     $("#name").keydown(function() {
+//         console.log("Key down")
+//     })
+
+//     $("#name").keyup(function() {
+//         console.log("Key up")
+//     })
+
+
+//     // Form events
+//     $("#fname").focus(function() {
+//         console.log("Focus field")
+//     })
+
+//     $("#fname").blur(function() {
+//         console.log("blur")
+//     })
+
+//     $("#form-id").submit(function(e) {
+//         console.log("Form submitted")
+//         e.preventDefault()
+
+//     })
+
+
+//     // Window event
+//     $(window).resize(function() {
+//         console.log("Window resized")
+//     })
+// })
